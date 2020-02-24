8d9740d289aadd3f9ffd6851f38bedd25eec9ac6
Kiran Junnarkar <kjunnarkar@yahoo.com>
2020-02-23 19:50:27 -0800

    Completed: App, ProtectedRoute, Login, axiosWithAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import BubblePage from './components/BubblePage';
 import Login from "./components/Login";
+import ProtectedRoute from './components/ProtectedRoute';
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <ul className='top-links'>
+          <li>
+            <Link className='list-links' to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link className='list-links' to='/bubbles'>See Bubbles</Link>
+          </li>
+        </ul>
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Route path='/login' component={Login} />
+        <ProtectedRoute exact path='/bubbles' component={BubblePage} />
+          {/* 
+            Build a PrivateRoute component that will 
+            display BubblePage when you're authenticated 
+          */}
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,68 @@
-import React from "react";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import "../styles.scss";
 
-const Login = () => {
+const Login = ({ history }) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [userLogin, setUserLogin] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = event => {
+
+    setUserLogin({ ...userLogin, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+      event.preventDefault();
+      axiosWithAuth()
+        .post('/login', userLogin)
+        .then(res => {
+            console.log('Here is the response from the Login Post', res.data.payload);
+            localStorage.setItem('token', res.data.payload);
+            setUserLogin({
+                username: '',
+                password: ''
+            })
+            history.push('/bubbles'); 
+        })
+        .catch(err => {
+            localStorage.removeItem('token');
+            console.log('Invalid username or password', err);
+        })
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <h1 className='top-heading'>Welcome to Kiran's Bubble App!</h1>
+      <h2>Enter Login Credentials</h2>
+        <form className='form-setup' onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username</label>
+            <input
+              className='enter-input'
+              id='username'
+              type='text'
+              name='username'
+              placeholder='Enter Username'
+              onChange={handleChange}
+              value={userLogin.username}
+            />
+            <label htmlFor='password'>Password</label>
+            <input
+              className='enter-input'
+              id='password'
+              type='password'
+              name='password'
+              placeholder='Enter Password'
+              onChange={handleChange}
+              value={userLogin.password}
+            />
+            <button className='submit-button' type='submit'>Log In</button>
+        </form>
+    </div>
   );
 };
 
